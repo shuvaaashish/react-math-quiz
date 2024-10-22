19fb4ae677defc4a414a84a103a9ee966e5baba6
shuvaaashish <gyawalishuva2007@gmail.com>
2024-10-22 19:10:16 +0545

    added fields on listing and created create page

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -9,6 +9,10 @@ class Auction_listings(models.Model):
     title=models.CharField(max_length=64)
     Owner=models.ForeignKey(User, on_delete=models.CASCADE , related_name="seller")
     description=models.CharField(max_length=350)
+    imgurl=models.CharField(max_length=99999)
+    startingbid = models.DecimalField(max_digits=10, decimal_places=2)
+
+
 
 class Bids(models.Model):
     pass
